da3d3152b44ec6b1289dcb1d025915329c795d94
Nhozip <huyenchien1996@gmail.com>
2017-01-17 16:52:50 +0700

    uapdte

--- b1.js
+++ b1.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('chào node js\n');
+   res.end('chào node   js\n');
 }).listen(777);
 
 console.log('Server running at 777');
