3eeab15f9eb760d5b7873afccd02aad38b3171ac
Nhozip <huyenchien1996@gmail.com>
2017-01-17 17:12:10 +0700

    del

--- bai2/b1.js
+++
@@ -1,8 +0,0 @@
-var http = require("http");
-
-http.createServer(function (req, res) {
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('chào node   js\n');
-}).listen(777);
-
-console.log('Server running at 777');
